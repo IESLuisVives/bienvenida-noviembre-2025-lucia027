d3bd6f08cacdc9c70134d620db041f9e42b64cc0
lucia027 <fuertes.lucia.27@gmail.com>
2025-11-18 09:45:50 +0100

    Añadido del proyecto HolaMundo

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Main (args);
+
+Console.WriteLine("Presiona una tecla para salir...👋");
+Console.ReadKey();
+void Main(string[] args) {
+    WriteLine("Hola Mundo");
+}
